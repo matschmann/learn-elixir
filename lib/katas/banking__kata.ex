56ef06152df01548193645dfce434f6165f1fd1e
Rainer Standke <standke@signal7.de>
2019-08-06 20:55:54 +0200

    Created new module to implement the bankin kata

---
+++ lib/katas/banking__kata.ex
@@ -0,0 +1,5 @@
+defmodule Banking_Kata do
+  @moduledoc false
+
+  def hello(name), do: "Hello, " <> name
+end
---
+++ test/katas/Banking_kata_test.exs
@@ -0,0 +1,8 @@
+defmodule Banking_KataTest do
+  use ExUnit.Case
+
+  test "greets the world" do
+    assert Banking_Kata.hello("lala") == "Hello, lala"
+  end
+
+end
